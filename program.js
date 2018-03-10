c9ee41e5d723649301236e2a63370c3de4a88a53
psx95 <pranav.ps95@hotmail.com>
2018-03-10 17:38:01 +0530

    Added Express

--- program.js
+++ program.js
@@ -1,6 +1,17 @@
 var book = require("./lib/grades").book;
-// starts at i = 2 since the first parameter is a string 'node' and the second argument is the name of the file including the file path
-for (var i = 2; i < process.argv.length; i++) {
-    book.addGrade (parseInt(process.argv[i]));
-}
-console.log(book.getAverage());
+var express = require("express");
+var app = express();
+app.get("/", function (req,res){
+    res.send("Hello World !");
+});
+app.get("/grade", function (req, res){
+    var grades = req.query.grades.split(",");
+    for (var i = 0; i<grades.length; i++) {
+        book.addGrade(parseInt(grades[i]));
+    }
+    var avg = book.getAverage();
+    var letter = book.computeLetterGrade();
+    res.send("Your average is "+avg+ " grade is "+letter);
+})
+app.listen (3000);
+console.log ("Server Ready...");
